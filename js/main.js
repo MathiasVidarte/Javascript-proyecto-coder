f723ea8f2545a2dbd4d173530816db26c8f92b18
MathiasVM <mathiasvidarte.m@gmail.com>
2023-03-13 23:58:17 -0300

    local storage operaciones

--- js/main.js
+++ js/main.js
@@ -8,33 +8,34 @@ const cambioEl = document.getElementById('cambio');
 const tazaEl = document.getElementById('taza');
 
 
-// Fetch Tipo de cambio y actualización del DOM
-function calculate(){
-    const moneda_one = monedaEl_one.value;
-    const moneda_two = monedaEl_two.value;
-   
-   return fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
-   .then(res => res.json() )
-   .then(data => {
-       const taza = data.rates[moneda_two];
-       
-       cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+// Fetch Tipo de cambio y actualizacin del DOM
+function calculate() {
+  const moneda_one = monedaEl_one.value;
+  const moneda_two = monedaEl_two.value;
 
-       cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+  return fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const taza = data.rates[moneda_two];
 
-       return {
+      cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
+      cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+
+      const resultado = {
         moneda_one,
         moneda_two,
         cantidad_one: cantidadEl_one.value,
         cantidad_two: cantidadEl_two.value,
         taza,
-          };
+      };
 
+      guardarResultadosEnLocalStorage(resultado);
+
+      return resultado;
     });
-   
 }
 
- 
 
 //Event listeners
 monedaEl_one.addEventListener('change', calculate);
@@ -53,60 +54,65 @@ taza.addEventListener('click', () =>{
 calculate();
 
 
-function agregar () {
-   // console.log("agregado");
-    function Cliente (nombre,edad,email){
-      this.nombre=nombre;
-      this.edad=edad;
-      this.email=email;
+
+
+function guardarResultadosEnLocalStorage(resultado) {
+  const resultadosGuardados = obtenerResultadosDeLocalStorage();
+  resultadosGuardados.push(resultado);
+  localStorage.setItem('resultados', JSON.stringify(resultadosGuardados));
+}
+
+ 
+ window.onload = function() {
+  const resultados = obtenerResultadosDeLocalStorage();
+  if (resultados.length > 0) {
+    // Si hay resultados guardados, mostrarlos en la pagina
+    for (const resultado of resultados) {
+      // Crear un elemento HTML para mostrar el resultado
+      const resultadoEl = document.createElement('div');
+      resultadoEl.innerText = `1 ${resultado.moneda_one} = ${resultado.taza} ${resultado.moneda_two}`;
+      // Agregar elemento a resultados
+      resultadosEl.appendChild(resultadoEl);
     }
-  
-  var nombreAgregar = document.getElementById("nombre").value;
-  //console.log(nombreAgregar);
-  var edadAgregar =document.getElementById("edad").value;
-  //console.log(edadAgregar);
-  var emailAgregar =document.getElementById("email").value;
-  //console.log(emailAgregar);
-
-  nuevoUsuario = new Cliente(nombreAgregar,edadAgregar,emailAgregar);
-  console.log(nuevoUsuario);
-  guardar();
   }
-
-  var baseDatos= [];
-  function guardar(){
-    baseDatos.push(nuevoUsuario);
-    console.log(baseDatos);
+}
+function obtenerResultadosDeLocalStorage() {
+  const resultadosGuardados = localStorage.getItem('resultados');
+  if (resultadosGuardados) {
+    return JSON.parse(resultadosGuardados);
   }
+  return [];
+}
 
-  function manejarclick () { 
-    alert ("Registrado con éxito")
-
-    BigInt.onclick = function(){alert("Registrado con éxito")}
-  }
-
-  function guardarResultadoEnLocalStorage() {
-    calculate().then(resultado => {
-        const resultadoArray = JSON.parse(localStorage.getItem('resultadoArray')) || [];
-        resultadoArray.push(resultado);
-        localStorage.setItem('resultadoArray', JSON.stringify(resultadoArray));
-    });
-}       
-
-
-   function obtenerResultadoDesdeLocalStorage() {
-   const resultadoArrayString = localStorage.getItem('resultadoArray');
-                          
-     if (resultadoArrayString) {
-      const resultadoArray = JSON.parse(resultadoArrayString);
-     console.log(resultadoArray);
-     } else {
-      console.log('No hay resultados guardados');
-                          }
-                      }
-
-
-  
-  
-
-  
+                      /*function agregar () {
+                        // console.log("agregado");
+                         function Cliente (nombre,edad,email){
+                           this.nombre=nombre;
+                           this.edad=edad;
+                           this.email=email;
+                         }
+                       
+                       var nombreAgregar = document.getElementById("nombre").value;
+                       //console.log(nombreAgregar);
+                       var edadAgregar =document.getElementById("edad").value;
+                       //console.log(edadAgregar);
+                       var emailAgregar =document.getElementById("email").value;
+                       //console.log(emailAgregar);
+                     
+                       nuevoUsuario = new Cliente(nombreAgregar,edadAgregar,emailAgregar);
+                       console.log(nuevoUsuario);
+                       guardar();
+                       }
+                     
+                       var baseDatos= [];
+                       function guardar(){
+                         baseDatos.push(nuevoUsuario);
+                         console.log(baseDatos);
+                       }
+                     
+                       function manejarclick () { 
+                         alert ("Registrado con éxito")
+                     
+                         BigInt.onclick = function(){alert("Registrado con éxito")}
+                       }*/
+                       
